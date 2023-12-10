ff9957ecb074ac61405694f570888735acda6237
Laura-Melina <laura.otzen@web.de>
2023-12-10 22:01:32 +0100

    ordered code a bit and added the count letters function

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-with open("books/frankenstein.txt") as f:
-    file_contents = f.read()
+
+def get_text():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+    return file_contents
+def count_words(file_contents):
     words = file_contents.split()
     wordcount = 0 
     for word in words: 
         wordcount += 1 
     print(wordcount)
+
+def count_letters():
+    text = get_text()
+    text = text.lower()
+    letter_dic = {}
+    for i in text: 
+        letter_dic[i] = 0   
+
+    for i in text: 
+        letter_dic[i] += 1 
+    print(letter_dic)
+count_letters()
